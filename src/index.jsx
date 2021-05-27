469441fb7196249f6e6c15c46b066041bb8c00fa
Artyom <seminartem98@mail.ru>
2021-05-27 09:24:09 +0700

    created webpack config and gitignore

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom';
+import React from 'react';
+import App from './App.jsx';
+
+import './styles/styles.css';
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.jsx'],
+    // app: ['@babel/polyfill', './app.jsx'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
